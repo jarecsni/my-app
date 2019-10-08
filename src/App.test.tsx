e4c04c7e8b5f65b5c3461c635aaf5185089ffa1b
Janos Jarecsni <janos.jarecsni@gmail.com>
2019-10-08 08:20:56 +0100

    Fix a test, add ts for eslint (not yet working)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { MemoryRouter as Router, Route } from 'react-router-dom';
 
 import App, { Home } from './App';
@@ -18,20 +18,11 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it.skip('renders a button', done => {
-  const wrapper = mount(
-    <Router initialEntries={['/']} initialIndex={0}>
-      <Route path="/" render={() => <Home />} />
-    </Router>
+it('renders a button', () => {
+  const home = shallow(<Home />);
+  expect(componentWithId(home, 'button-look')).not.toBeNull();
+  expect(componentWithId(home, 'button-look')).toHaveLength(1); // this returns 2 for some reason when inside a router
+  expect(componentWithId(home, 'button-look').text()).toEqual(
+    "Look, I'm a button!"
   );
-  process.nextTick(() => {
-    const home = wrapper.find(Home);
-    console.log(home.html());
-    expect(componentWithId(home, 'button-look')).not.toBeNull();
-    expect(componentWithId(home, 'button-look')).toHaveLength(1); // this returns 2 for some reason
-    expect(componentWithId(home, 'button-look').text()).toEqual(
-      "Look, I'm a button!"
-    );
-    done();
-  });
 });
