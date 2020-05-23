72a6a1e247e2d1cc8b6605bae2c271edc3ff4028
Janos Jarecsni <janos.jarecsni@gmail.com>
2020-05-23 11:43:45 +0100

    Prettier problems...

--- src/services/TimeService.ts
+++ src/services/TimeService.ts
@@ -1,12 +1,12 @@
 import { PubSubService } from './PubSubService';
 
 class TimeService extends PubSubService {
-  constructor() {
-    super('time');
-    setInterval(() => {
-      super.publish(new Date().getTime());
-    }, 100);
-  }
+    constructor() {
+        super('time');
+        setInterval(() => {
+            super.publish(new Date().getTime());
+        }, 100);
+    }
 }
 
 export const timeService = new TimeService();
